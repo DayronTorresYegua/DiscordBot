1f9e70c2d36978f8c152462e8a7190e59205fcc5
DayronTorresYegua <dtorresy@iesrafaelalberti.es>
2025-02-18 09:27:01 +0100

    Estructura proyecto y avances

---
+++ api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastappi.staticfiles import StaticFiles
+import json
+
+app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+with open("data.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+@app.get("/personajes/{nombre}")
+async def get_personaje(nombre: str):
+    pesonaje = next((pj for pj in data["personajes"] if pj["nombre"].lower() == nombre), None)
+    if pesonaje:
+        personaje["imagen"] = f"/assets/{personaje['imagen']}"
+        return personaje
+    else:
+        return {"error": "Personaje no encontrado"}
