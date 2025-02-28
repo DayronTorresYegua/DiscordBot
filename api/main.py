af34e98621a330b3c42754b24bae3ed39ae1cbeb
DayronTorresYegua <dtoryeg519@g.educaand.es>
2025-02-28 23:51:05 +0100

    configuraciones

--- api/main.py
+++ api/main.py
@@ -4,15 +4,15 @@ import json
 
 app = FastAPI()
 
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount("/assets", StaticFiles(directory="api/assets"), name="assets")
 
-with open("data.json", "r", encoding="utf-8") as file:
+with open("api/data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
 @app.get("/personajes/{nombre}")
 async def get_personaje(nombre: str):
     personaje = next((pj for pj in data["personajes"] if pj["nombre"].lower() == nombre), None)
     if personaje:
-        personaje["imagen"] = f"/assets/{personaje['imagen']}"
+        personaje["imagen"] = f"/assets/{personaje['imagen']}"  
         return personaje
     return {"error": "Personaje no encontrado"}
--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+# Cargar variables de entorno desde el archivo .env
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-API_URL = os.getenv("API_URL")
+
+# Definir las variables de configuración
+TOKEN = os.getenv("DISCORD_TOKEN")  # Token del bot
+API_URL = os.getenv("API_URL")  # URL de la API
