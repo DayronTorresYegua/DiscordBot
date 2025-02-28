8b1c74c370240a5868a614d7dff04ff8331e1d1f
DayronTorresYegua <dtoryeg519@g.educaand.es>
2025-02-28 22:31:52 +0100

    Ajustes en la estructura y actualizacion del codigo

--- api/main.py
+++ api/main.py
@@ -4,20 +4,15 @@ import json
 
 app = FastAPI()
 
-# Montar la carpeta de imágenes
 app.mount("/assets", StaticFiles(directory="api/assets"), name="assets")
 
-# Cargar datos de los personajes
 with open("api/data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
-# Endpoint para obtener informacion de un personaje
 @app.get("/personajes/{nombre}")
 async def get_personaje(nombre: str):
     personaje = next((pj for pj in data["personajes"] if pj["nombre"].lower() == nombre.lower()), None)
-    
     if personaje:
         personaje["imagen"] = f"/assets/{personaje['imagen']}"
         return personaje
-    else:
-        return {"error": "Personaje no encontrado"}
+    return {"error": "Personaje no encontrado"}
---
+++ bot/config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+API_URL = os.getenv("API_URL")
