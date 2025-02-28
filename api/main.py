df21b847846f6971412a6a5677ccf18ee1bb7d57
DayronTorresYegua <dtoryeg519@g.educaand.es>
2025-02-28 23:04:51 +0100

    Ajustes repositorio

--- api/main.py
+++ api/main.py
@@ -4,19 +4,15 @@ import json
 
 app = FastAPI()
 
-# Montar la carpeta de imágenes
-app.mount("/assets", StaticFiles(directory="api/assets"), name="assets")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
-# Cargar datos de los personajes
-with open("api/data.json", "r", encoding="utf-8") as file:
+with open("data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
 @app.get("/personajes/{nombre}")
 async def get_personaje(nombre: str):
-    personaje = next((pj for pj in data["personajes"] if pj["nombre"].lower() == nombre.lower()), None)
-    
+    personaje = next((pj for pj in data["personajes"] if pj["nombre"].lower() == nombre), None)
     if personaje:
-        personaje = personaje.copy()  # Copia para no modificar el JSON original
         personaje["imagen"] = f"/assets/{personaje['imagen']}"
         return personaje
     else:
